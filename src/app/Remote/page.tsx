050eb7f19f01cb159e69ade07a7f96a891419ccb
souri-t <multiplatform.world@gmail.com>
2023-02-22 01:03:55 +0900

    first commit

--- src/app/Remote/page.tsx
+++ src/app/Remote/page.tsx
@@ -32,6 +32,28 @@ function Remote() {
       fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+        const responce = await fetch('https://smarthome-server.hasura.app/api/rest/remote/appliances');
+        setAppliances((await responce.json()).appliances);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        if (!applianceId) return;
+        const responce = await fetch(`https://smarthome-server.hasura.app/api/rest/remote/appliances/${applianceId}/commands`);
+        setCommands((await responce.json()).remote_commands);
+    };
+    fetchData();
+  }, [applianceId]);
+
+  async function onClickButton() {
+    console.log(commandId);
+    addLogs({applience : "a", command : "c"} as Log)
+  }
+
   return (
     <main>
         <div id="content" style={{ padding:"10px;"}} className="table-responsive">
@@ -41,8 +63,8 @@ function Remote() {
                 <div className="col-sm-4">
                     <div className="">
                         <label className="control-label col-xs-2"><b>IR FileName:</b></label>
-                          <div className="col-xs-5">
-                          <select className="form-control input-sm" onChange={(e) => setApplianceId(e.target.value)}>
+                        <div className="col-xs-5">
+                            <select className="form-control input-sm" onChange={(e) => setApplianceId(e.target.value)}>
                                 <option value="">-</option>
                                 {appliances.map((appliance) => (
                                     <option key={appliance.id} value={appliance.id}>{appliance.name}</option>
@@ -55,6 +77,12 @@ function Remote() {
                     <div className="">
                         <label className="control-label col-xs-2"><b>IR Command:</b></label>
                         <div className="col-xs-5">
+                            <select className="form-control input-sm" onChange={(e) => setCommandId(e.target.value)}>
+                            <option value="">-</option>
+                                {commands.map((command) => (
+                                    <option key={command.id} value={command.id}>{command.name}</option>
+                                ))}
+                            </select>
                         </div>
                     </div>
                 </div>
@@ -62,6 +90,7 @@ function Remote() {
                     <div className="">
                         <label className="control-label col-xs-2"><b>Control:</b></label>
                         <div className="col-xs-5">
+                            <button className="btn btn-success btn-sm" onClick={() => onClickButton()}>execute</button>
                         </div>
                     </div>
                 </div>
@@ -78,10 +107,18 @@ function Remote() {
                 </tr>
             </thead>
             <tbody>
+                {logs.map((log) => (
+                    <tr>
+                        <td><div id="date"></div></td>
+                        <td><div id="sendedfilename">{log.applience}</div></td>
+                        <td><div id="sendedcommand">{log.command}</div></td>
+                        <td><div id="result"></div></td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     </main>
-)
+  )
 }
 
 export default Remote;
